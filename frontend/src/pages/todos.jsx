c784f7e25c919321e685256b1065623d5dd17e47
Esme577 <esmeraldaquezada57@gmail.com>
2025-07-20 17:57:45 -0700

    added the mark as complete to the todoitem list

--- frontend/src/pages/todos.jsx
+++ frontend/src/pages/todos.jsx
@@ -7,6 +7,7 @@ import axios from "axios";
 export default function Todos(){
 
 const modalRef = useRef();
+const queryClient = useQueryClient();
 
 const { mutate: createNewTodo } = useMutation({
 	  // The key used to identify this mutation in React Query's cache
@@ -23,6 +24,49 @@ const { mutate: createNewTodo } = useMutation({
 	    return data;
 	  },
   });
+const { mutate: markAsCompleted } = useMutation({
+  mutationKey: ["markAsCompleted"],
+  mutationFn: async (todoId) => {
+   const axiosInstance = await getAxiosClient(); 
+
+    const { data } = await axiosInstance.put(`http://localhost:8080/todos/${todoId}/completed`);
+
+    return data;
+  },
+  onSuccess: () => {
+    queryClient.invalidateQueries("todos");
+  }
+});
+  
+
+const { data, isError, isLoading } = useQuery({
+  // A unique key to identify this query in React Query's cache
+  queryKey: ["todos"],
+
+  // The function responsible for fetching the data
+  queryFn: async () => {
+    const axiosInstance = await getAxiosClient();
+
+    // Use the Axios instance to send a GET request to fetch the list of todos
+    const { data } = await axiosInstance.get("http://localhost:8080/todos");
+
+    // Return the fetched data (React Query will cache it under the queryKey)
+    return data;
+  },
+});
+
+  if(isLoading){
+    return (
+      <div>Loading Todos...</div>
+    )
+  }
+  
+  if(isError){
+    return (
+      <div>There was an error</div>
+    )
+  }
+
 
 const toggleNewTodoModal = () => {
   console.log("modalaction", modalRef.current)
@@ -89,7 +133,7 @@ function TodoModal ({ modalRef}) {
               <button type="submit" className="btn btn-primary">
                 Create Todo
               </button>
-              <button type="button" className="btn btn-ghost">
+              <button type="button" onClick={toggleNewTodoModal} className="btn btn-ghost">
                 Close
               </button>
             </div>
@@ -99,11 +143,44 @@ function TodoModal ({ modalRef}) {
 )
   
 }
+function TodoItemList ({data, markAsCompleted}){
+  return (
+  <div className="w-lg h-sm flex column items-center justify-center gap-4">
+    {data.success && data.todos && data.todos.length >= 1 && (
+      <ul className="flex column items-center justify-center gap-4">
+        {
+          data.todos.map(todo => (
+            <li className="inline-flex items-center gap-4">
+              <div className="w-md">
+                <h3 className="text-lg">
+                  {todo.name}
+                </h3>
+                <p className="text-sm">{todo.description}</p>
+              </div>
+              <div className="w-md">
+                <label className="swap">
+                  <input type="checkbox" onClick={() => markAsCompleted(todo.id)} />
+                  <div className="swap-on">
+                    Yes
+                  </div>
+                  <div className="swap-off">
+                    No
+                  </div>
+                </label>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+    )}</div>
+)
+}
 
 
   return (
     <> 
     <NewTodoButton toggleNewTodoModal={toggleNewTodoModal} />
+    <TodoItemList data={data} markAsCompleted={markAsCompleted}/>
     <TodoModal modalRef={modalRef} />
     </>
   )
